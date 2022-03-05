3c00912e48c9753b9d7b294dd52f240a9de136dd
andylolu2 <andylolu24@gmail.com>
2022-03-05 02:18:10 +0000

    Add prometheus and grafana monitoring

--- src/api/main.py
+++ src/api/main.py
@@ -1,3 +1,10 @@
+from prometheus_fastapi_instrumentator import Instrumentator
+
 from api.app import create_app
 
 app = create_app()
+
+Instrumentator(
+    should_group_status_codes=False,
+    excluded_handlers=["/metrics"],
+).instrument(app).expose(app)
