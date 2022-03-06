5d76e4db7d0a989512400aa35fba7de8bb8ad59f
andylolu2 <andylolu24@gmail.com>
2022-03-06 02:08:28 +0000

    Log latency metric with handlers

--- src/api/main.py
+++ src/api/main.py
@@ -1,4 +1,4 @@
-from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_fastapi_instrumentator import Instrumentator, metrics
 
 from api.app import create_app
 
@@ -7,4 +7,11 @@ app = create_app()
 Instrumentator(
     should_group_status_codes=False,
     excluded_handlers=["/metrics"],
+).add(
+    metrics.requests(should_include_method=False)
+).add(
+    metrics.latency(
+        should_include_method=False,
+        buckets=(0.01, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10)
+    )
 ).instrument(app).expose(app)
