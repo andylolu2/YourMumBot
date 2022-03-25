87f2afdffcb8565d48521381bedffa248a2a6b9e
andylolu2 <andylolu24@gmail.com>
2022-03-25 22:08:30 +0000

    Add timer helper class for logging

--- src/api/model.py
+++ src/api/model.py
@@ -1,20 +1,22 @@
 from __future__ import annotations
-from typing import List, Awaitable
+from typing import List, Tuple
 import functools
 import logging
 import re
 import asyncio
 
-import detoxify
+
 import nltk
 import numpy as np
 from stanza.server.client import logger as stanza_logger
 from stanza.server import CoreNLPClient
 from stanza.server import StartServer
+from detoxify import Detoxify
 
 from api import CORENLP_ENDPOINT, CORENLP_TIMEOUT, WARM_UP_TEXT, MAX_SUB_SAMPLES
 from api.corrector import LanguageToolCorrector
 from api.logger import logger
+from helper.timer import timer
 
 stanza_logger.setLevel(logging.ERROR)
 
@@ -23,9 +25,8 @@ class YourMumModel():
     _your_mum_node = nltk.tree.Tree('NP', ['your mum'])
 
     def __init__(self) -> None:
+        self._model = Detoxify('original-small')
         self._corrector = LanguageToolCorrector()
-
-        self._model = detoxify.Detoxify('original-small')
         self._corenlp_client = CoreNLPClient(
             start_server=StartServer.DONT_START,
             endpoint=CORENLP_ENDPOINT,
@@ -42,7 +43,7 @@ class YourMumModel():
         self._corenlp_client.__exit__(*args)
 
     # public methods
-    def yourmumify(self, text: str, log: bool = True) -> List[str]:
+    def yourmumify(self, text: str, log: bool = True) -> Tuple[List[str], List[float]]:
         '''
         Creates your mum jokes from given text.
         :param text: Text to yourmumify.
@@ -53,7 +54,8 @@ class YourMumModel():
         if log:
             logger.debug(f'Input: {text}')
 
-        ann = self._corenlp_client.annotate(text)
+        with timer(logger=logger, prefix="CoreNLP latency: "):
+            ann = dict(self._corenlp_client.annotate(text))
         for sent in ann["sentences"]:
             tree = nltk.tree.Tree.fromstring(sent["parse"])
             if log:
@@ -68,14 +70,8 @@ class YourMumModel():
             if len(yourmumified) == 0:
                 continue
 
-            # check if list is non-empty
-            for i, yourmum_sent in enumerate(yourmumified):
-                # correct grammatical mistakes
-                yourmumified[i] = self._correct_grammar(yourmum_sent)
-
-            # evaluate toxicity of augmented setence,
-            # find the most toxic one
-            scores = self._toxic_score(yourmumified, key="toxic")
+            # evaluate toxicity of augmented setence, find the most toxic one
+            scores = self._toxic_score(yourmumified, key="toxicity")
             if log:
                 logger.debug(
                     f'Detoxify scores: {list(zip(yourmumified, scores))}'
@@ -83,7 +79,6 @@ class YourMumModel():
             best = yourmumified[np.argmax(scores)]
             best_score = max(scores)
 
-            # apply grammar corrector on final output to catch more errors
             best = self._correct_grammar(best)
             bests.append(best)
             best_scores.append(best_score)
@@ -95,7 +90,9 @@ class YourMumModel():
         '''
         self.yourmumify(WARM_UP_TEXT, log=False)
 
-    async def async_yourmumify(self, text: str, log: bool = True) -> Awaitable[List[str]]:
+    async def async_yourmumify(self,
+                               text: str,
+                               log: bool = True) -> Tuple[List[str], List[float]]:
         '''
         Executes yourmumify asynchronously.
         '''
@@ -159,7 +156,8 @@ class YourMumModel():
         :param str: One of {toxic, severe_toxic, obscene, threat, insult, identity_hate}.
         :return: List of toxicity scores. Higher is more toxic.
         '''
-        scores = self._model.predict(texts)[key]
+        with timer(logger=logger, prefix="Detoxify latency: "):
+            scores = self._model.predict(texts)[key]
         return scores
 
     def _correct_grammar(self, text: str) -> str:
@@ -168,4 +166,5 @@ class YourMumModel():
         :param text: Text to correct grammar.
         :return: String with corrected grammar.
         '''
-        return self._corrector.correct(text)
+        with timer(logger=logger, prefix="Corrector latency: "):
+            return self._corrector.correct(text)
