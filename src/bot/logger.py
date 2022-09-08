9ece251bf02840dbd55505e4bc85e2c2834888f8
andylolu2 <andylolu24@gmail.com>
2022-09-08 17:52:15 +0100

    Change request id counter

--- src/bot/logger.py
+++ src/bot/logger.py
@@ -1,10 +1,10 @@
 from bot import LOG_FORMAT, LOG_LEVEL
-from bot.contexts import request_id
+from bot.contexts import get_request_id
 from helper.logger import get_logger
 
 logger = get_logger(
     name=__name__,
     log_format=LOG_FORMAT,
     level=LOG_LEVEL,
-    contextvars=[("request_id", request_id)],
+    callbacks=[("request_id", get_request_id)],
 )
